033915439a30613c9afb016b89dbf5e42bb19641
Arthur32p <arthurlelisuchoa32@gmail.com>
2026-03-06 20:16:36 -0300

    initial commit

---
+++ src/main/java/io/github/arthur/Fintrack/FintrackApplication.java
@@ -0,0 +1,13 @@
+package io.github.arthur.Fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackApplication.class, args);
+	}
+
+}
