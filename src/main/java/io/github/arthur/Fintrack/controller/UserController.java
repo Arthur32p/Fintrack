42e2ed6256affaa5ea1d55b87a21f52f845d0ca6
Arthur32p <arthurlelisuchoa32@gmail.com>
2026-03-07 08:34:24 -0300

    chore(user): add controller

---
+++ src/main/java/io/github/arthur/Fintrack/controller/UserController.java
@@ -0,0 +1,16 @@
+package io.github.arthur.Fintrack.controller;
+
+import io.github.arthur.Fintrack.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+
+
+}
